b504baba2e3b8d70a6ca513cc1d9918187b6c8b1
= <=>
2021-11-27 15:03:11 +0530

    Longer message

--- app.mjs
+++ app.mjs
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Wetin dey sup')
+  res.send('Wetin dey sup? Wahala be for thos niggas who no sabi deploying to heroku')
 })
 
 app.listen(port, () => {
